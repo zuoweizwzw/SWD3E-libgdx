a7ecb196aa8eed5f56b6207e1810a647cad1f399
zuoweizwzw <zuoweizwzw@gmail.com>
2015-05-17 16:48:57 +0200

    test

--- SWD3E/src/zw/swd/gui/GUIItemList.java
+++ SWD3E/src/zw/swd/gui/GUIItemList.java
@@ -1,15 +1,23 @@
 package zw.swd.gui;
 
+import java.util.ArrayList;
+
 import zw.swd.game.GameModel;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class GUIItemList extends List{
 
 	GameModel gameModel;
+	GUIItemListScroll scroll;
 	public GUIItemList(GameModel gameModel) {
 		// TODO Auto-generated constructor stub
 		this.gameModel=gameModel;
+		scroll=new GUIItemListScroll();
+		scroll.setPosition(185, 0);
+		this.setName("guiitemlist");
+		this.addActor(scroll);
 	}
 
 	@Override
@@ -18,15 +26,23 @@ public class GUIItemList extends List{
 		
 	}
 	
-	public void fillData()
-	{
-		
-	}
+	
 
 	@Override
 	public void fillData(Object object) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public ArrayList<FightMagicItem> getItems()
+	{
+		ArrayList<FightMagicItem> items=new ArrayList<>();
+		ArrayList<Actor> actors=this.findActors(FightMagicItem.class);
+		for(Actor actor:actors)
+		{
+			items.add((FightMagicItem)actor);
+		}
+		return items;
 	}
 
 }
--- SWD3E/src/zw/swd/gui/List.java
+++ SWD3E/src/zw/swd/gui/List.java
@@ -1,6 +1,9 @@
 package zw.swd.gui;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public abstract class List extends Paper{
 
